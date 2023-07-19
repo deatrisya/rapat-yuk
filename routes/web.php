f5789e15d6ab596506cd78da51361ff73214d901
sitiaisyah14 <sitiaisyah4110@gmail.com>
2023-07-19 13:36:14 +0700

    route room

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.admin.dashboard');
 });
+
+Route::resource('room', RoomController::class);
+Route::post('/room-data', [RoomController::class,'data']);
