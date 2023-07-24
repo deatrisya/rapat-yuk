a7fa9fffebe82057d45ea2d478bb01c46b409b5e
ilham nayoko <nayokoilham@gmail.com>
2023-07-24 15:11:07 +0700

    intial commit

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,41 +1,25 @@
     <div class="card mb-4">
-            <div class="card-body">
-                <div class="card-title">
-                    <div class="avatar flex-shrink-0">
-                        <div class="avatar flex-shrink-0">
-                            {{-- <img src="../assets/img/icons/unicons/chart-success.png" alt="chart success"
-                                class="rounded" /> --}}
-                        </div>
-                        {{-- <div class="dropdown">
-                            <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                <i class="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                                <a class="dropdown-item" href="javascript:void(0);">View
-                                    More</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                            </div>
-                        </div> --}}
-                    </div>
-                    {{-- <div class="dropdown">
-                        <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i class="bx bx-dots-vertical-rounded"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                            <a class="dropdown-item" href="javascript:void(0);">View
-                                More</a>
-                            <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                        </div>
-                    </div> --}}
-                    <span class="fw-semibold d-block mb-1">@isset($title)
-                        {{ $title }}
-                    @endisset</span>
-                    <h3 class="card-title mb-2">@isset($value)
-                        {{ $value }}
-                    @endisset</h3>
-                    <a href="#" class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i>Meningkat</a>
+        <div class="row">
+                <div class="d-flex mx-auto justify-content-center align-items-center col-3 my-3 rounded-3 card-icon @isset($bg_color) {{ $bg_color }} @endisset">
+                    <span class="d-inline-block text-white fs-1 @isset($icon) {{ $icon }} @endisset"></span>
                 </div>
-            </div>
+                <div class="card-wrap col">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <span class="fw-semibold d-block mb-1">@isset($title) {{ $title }} @endisset</span>
+                            <h3 class="card-title mb-2">@isset($nominal) {{ $nominal }} @endisset</h3>
+                            {{-- <div class="dropdown">
+                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    @isset($nominal) {{ $nominal }} @endisset
+                                </a>
+                                <ul class="dropdown-menu">
+                                  <li><a class="dropdown-item" href="#">Action</a></li>
+                                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div> --}}
+                        </div>
+                    </div>
+                </div>
+        </div>
     </div>
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -5,20 +5,29 @@
     <div class="row">
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
             @component('components.card')
+            @slot('bg_color', 'bg-success')
+            @slot('icon', 'bx bxs-home-heart ')
             @slot('title', 'Jumlah Ruang Tersedia')
-            @slot('value', '3')
+            @slot('nominal', '3')
+            @slot('ruang', 'Ruang Welirang')
             @endcomponent
         </div>
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
             @component('components.card')
+            @slot('bg_color', 'bg-info')
+            @slot('icon', 'bx bxs-home ')
             @slot('title', 'Jumlah Ruangan')
-            @slot('value', '3')
+            @slot('nominal', '3')
+            @slot('ruang', 'Ruang Welirang')
             @endcomponent
         </div>
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
             @component('components.card')
+            @slot('bg_color', 'bg-warning')
+            @slot('icon', 'bx bxs-user-account ')
             @slot('title', 'Jumlah Pengguna')
-            @slot('value', '10')
+            @slot('nominal', '10')
+            @slot('ruang', 'Ruang Welirang')
             @endcomponent
         </div>
     </div>
