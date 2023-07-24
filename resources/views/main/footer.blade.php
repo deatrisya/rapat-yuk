f6a6c8834b6c0e4e1a3f080a5275d7eabfd5074d
deatrisya <mirelldee47@gmail.com>
2023-07-24 10:40:19 +0700

    change title web

--- resources/views/main/footer.blade.php
+++ resources/views/main/footer.blade.php
@@ -6,17 +6,10 @@
                 document.write(new Date().getFullYear());
             </script>
             , made with ❤️ by
-            <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+            <p class="footer-link fw-bolder">RapatYuk</p>
         </div>
         <div>
-            <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-            <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
 
-            <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank"
-                class="footer-link me-4">Documentation</a>
-
-            <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
-                class="footer-link me-4">Support</a>
         </div>
     </div>
 </footer>
